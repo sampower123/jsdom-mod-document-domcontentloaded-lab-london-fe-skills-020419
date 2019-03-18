f4bcead0b2d575267120c2a5c9050c5696ee8299
Sam Power <sam.g.power@gmail.com>
2019-03-18 19:46:03 +0000

    Automatically backed up by Learn

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-document.querySelector("body").addEventListener("click", e => console.log("Reggae, Reggae!"));
+
+
+document.addEventListener("DOMContentLoaded", e => {
+  document.querySelector("body")
+   .addEventListener("click", e => console.log("Reggae, Reggae!"));
+})
