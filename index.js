b8343c1b2c6a3205dd4c1d6e2eb8dacaa2a53453
Sam Power <sam.g.power@gmail.com>
2019-03-18 19:57:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ document.addEventListener("DOMContentLoaded", e => {
   document.querySelector("div#hidden-div").innerText = "301"
    .addEventListener("click", e => console.log("Reggae, Reggae!"));
 })
+
+var element = document.getElementById("id01");
+element.innerHTML = "New Heading";
