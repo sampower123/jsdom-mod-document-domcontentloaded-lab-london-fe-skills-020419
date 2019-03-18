76740f46822a1621d92a29bf73a00804e1244f5a
Sam Power <sam.g.power@gmail.com>
2019-03-18 20:00:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 /* code goes here */
 
-document.addEventListener("DOMContentLoaded", e => {
-  document.querySelector("div#hidden-div").innerText = "301"
-   .addEventListener("click", e => console.log("Reggae, Reggae!"));
-})
 
-var element = document.getElementById("id01");
-element.innerHTML = "New Heading";
+
+var element = document.getElementById("hidden-div");
+element.innerText = "301";
