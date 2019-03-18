2c6f5c3d1b149fbb988777cbdb44dbb17c373349
Sam Power <sam.g.power@gmail.com>
2019-03-18 19:55:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* code goes here */
 
 document.addEventListener("DOMContentLoaded", e => {
-  document.querySelector("div#hidden-div")
+  document.querySelector("div#hidden-div").innerText
    .addEventListener("click", e => console.log("Reggae, Reggae!"));
 })
